23786a54d2fb0f40821c7aed90b616f3241a50a9
charcharus <achrifiansyahfarrel@gmail.com>
2020-07-09 21:26:47 +0700

    final project

--- app/VoteJawaban.php
+++ app/VoteJawaban.php
@@ -8,6 +8,11 @@ class VoteJawaban extends Model
 {
     protected $table = 'vote_jawaban';
 
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
     public function jawaban()
     {
     	return $this->belongsTo(Jawaban::class);
--- app/VotePertanyaan.php
+++ app/VotePertanyaan.php
@@ -8,6 +8,11 @@ class VotePertanyaan extends Model
 {
     protected $table = 'vote_pertanyaan';
 
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
     public function pertanyaan()
     {
     	return $this->belongsTo(Pertanyaan::class);
